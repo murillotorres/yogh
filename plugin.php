0f793b9b1e255dec41b9c2d580f22a29eff6fc64
murillotttorres@live.com <murillotttorres@live.com>
2025-01-21 12:40:07 -0300

    Remove anonymous functions. Renames the function

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Client Customization
  * Plugin URI:      https://www.yogh.com.br/
- * Description:     Plugins with Project Customization
+ * Description:     Plugin with Project Customization
  * Author:          Yogh Soluções
  * Author URI:      https://www.yogh.com.br/
  * Text Domain:     client-customization
@@ -12,17 +12,17 @@
  * @package         Client_Customization
  */
 
-// If this file is called directly, abort.
+
 if ( ! defined( 'ABSPATH' ) ) {
-    die( 'not allowed' );
+    exit; 
 }
 
-add_filter( 'the_content', function ( $content ) {
-    // Verifica se estamos em uma publicação (post).
+function client_customization_add_message_to_content( $content ) {
     if ( is_singular( 'post' ) ) {
-        $message = '<p><b>This content is created by: ' . get_bloginfo( 'name' ) . ' (' . get_bloginfo( 'url' ) . ')</b></p>';
+        $message = '<p><b>This content is created by: ' . esc_html( get_bloginfo( 'name' ) ) . ' (' . esc_url( get_bloginfo( 'url' ) ) . ')</b></p>';
         return $content . $message;
     }
 
     return $content;
-}, 10 );
+}
+
